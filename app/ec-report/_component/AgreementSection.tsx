a4f3574e1119babc5be8ba251ce7effcce61405d
San-Goon <tigerlove95@naver.com>
2024-09-06 13:52:48 +0900

    feat: make agreement section

---
+++ app/ec-report/_component/AgreementSection.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import style from "./agreementSection.module.css";
+import { useCallback, useState } from "react";
+
+export default function AgreementSection() {
+  const [isChecked, setChecked] = useState(false);
+
+  const handleClickCheckbox = useCallback(() => {
+    setChecked(!isChecked);
+  }, [isChecked]);
+
+  const handleClickButton = useCallback(() => {
+    console.log("clicked");
+  }, []);
+
+  return (
+    <div className={style.container}>
+      Once sent, the report is final and cannot be retrieved. The counselor is
+      solely responsible for any incorrections in the report.
+      <div className={style.agreementSection}>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleClickCheckbox}
+        />
+        I agree to the above
+      </div>
+      <div
+        className={isChecked ? style.button : style.disabled}
+        onClick={isChecked ? handleClickButton : undefined}
+      >
+        Send to Student
+      </div>
+    </div>
+  );
+}
